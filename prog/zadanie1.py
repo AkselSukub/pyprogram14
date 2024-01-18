2be03874af21aec2ecd8a163a0bf6abe5d33595c
AkselSukub <aksel.sukubov@yandex.ru>
2024-01-18 04:38:56 +0300

    finot

---
+++ prog/zadanie1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def collection_converter(type):
+    def convert_to_collection(numbers):
+        if type == 'list':
+            collection = list(map(int, numbers.split()))
+        else:
+            collection = tuple(map(int, numbers.split()))
+        return collection
+
+    return convert_to_collection
+
+
+type_input = input("Введите тип коллекции ('list' или 'tuple'): ")
+numbers_input = input("Введите список целых чисел, записанных через пробел: ")
+
+converter = collection_converter(type_input)
+result = converter(numbers_input)
+print(result)
